9407b147079a3b88379bd19b35652a24f9d89f9c
kanhereashwin <kanhereashwin@users.noreply.github.com>
2021-09-22 14:19:31 -0700

    Added ephemeris and baseline files

--- src/gtsam/gtsam_toolbox/+gtsam/+utilities/extractPoint2.m
+++
@@ -1,6 +0,0 @@
-function varargout = extractPoint2(varargin)
-      if length(varargin) == 1 && isa(varargin{1},'gtsam.Values')
-        varargout{1} = gtsam_wrapper(2461, varargin{:});
-      else
-        error('Arguments do not match any overload of function gtsam.utilities.extractPoint2');
-      end
--- src/gtsam/gtsam_toolbox/gtsam_tests/thinBayesTree.m
+++
@@ -1,5 +0,0 @@
-function bayesTree = thinBayesTree(depth, width)
-    import gtsam.*
-    bayesNet = thinTreeBayesNet(depth, width);
-    bayesTree = GaussianBayesTree(bayesNet);
-end
